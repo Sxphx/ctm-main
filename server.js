eba8d25d7cb1b276f90174b688a22f819aa98323
Supphanat Deawwanich <90847280+Sxphx@users.noreply.github.com>
2025-04-12 22:58:52 +0700

    Update server.js

--- server.js
+++ server.js
@@ -30,10 +30,10 @@ app.use(
     resave: false,
     saveUninitialized: false,
     cookie: {
-      secure: false,
+      secure: true,
       maxAge: 24 * 60 * 60 * 1000,
       httpOnly: true,
-      sameSite: "lax",
+      sameSite: "none",
     },
   })
 );
